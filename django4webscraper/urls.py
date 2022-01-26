0084a6a4c7d4d5a9aacf052ae5f84c3b164fc970
Callum Ogle <callumogle@gmail.com>
2022-01-26 21:03:49 +0000

    added loading widget

--- django4webscraper/urls.py
+++ django4webscraper/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('webscraper/', include('webscraper.urls')),
+    path('', include('webscraper.urls')),
     path('admin/', admin.site.urls),
 ]
